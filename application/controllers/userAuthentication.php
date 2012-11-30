64dc66f59d98cae30552265e3ccf853893ac3b88
azadsalam <azadsalam2611@gmail.com>
2012-11-30 13:31:09 +0600

    LOG IN FOR ANDROID

--- application/controllers/userAuthentication.php
+++ application/controllers/userAuthentication.php
@@ -11,9 +11,24 @@ class UserAuthentication extends CI_Controller
         public function getNativeId($mail,$password)
         {
             $this->load->model('user_model');
-            $nid=$this->user_model->getNativeId($mail,$password);  
+            $nid=$this->user_model->getNativeId($mail,$password);
+            return $nid;
         }
 
+
+        public function androidQuery()
+        {
+                         $json=$_SERVER['HTTP_JSON'];
+                         $data=json_decode($json);
+
+
+                          $mail=$data->{'mail'};
+                          $password = $data->{'password'};
+
+                          $ret['nid'] = $this->getNativeId($mail, $password);
+
+                          print_r(json_encode($ret));
+        }
 
 
 }
