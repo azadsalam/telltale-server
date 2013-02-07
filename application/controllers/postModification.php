ef125cfee477278c9147129cd04e08dff775283b
azadsalam <azadsalam2611@gmail.com>
2013-02-08 01:21:31 +0600

    DELETE

--- application/controllers/postModification.php
+++ application/controllers/postModification.php
@@ -10,9 +10,21 @@ class PostModification extends CI_Controller
 		//$this->post_delete(37);		
 		
 	}
-	
-	
-	public function post_delete($pid)//kono ekta post id dile oi post shoho tar nicher shb post delete kore dibe
+
+
+        public function post_delete_from_android()
+        {
+                         $json=$_SERVER['HTTP_JSON'];
+                         $data=json_decode($json);
+
+
+                          $pid=intval($data->{'pid'});
+                          $this->post_delete($pid);
+
+        }
+
+
+        public function post_delete($pid)//kono ekta post id dile oi post shoho tar nicher shb post delete kore dibe
 	{
 		$this->load->model('post_model');
 		$this->post_model->post_delete($pid);
